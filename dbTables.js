c397ffe9fcf830b2e94b4458bce4d84791a63ca5
jerxmy <158511710+jerxmy@users.noreply.github.com>
2024-10-10 15:26:29 +0200

    settings

--- dbTables.js
+++ dbTables.js
@@ -1,24 +1,26 @@
 let mysql = require("mysql");
+let config = require("./config.json");
 
 let con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "migrationTest",
+  host: config.DB_HOST,
+  user: config.DB_USERNAME,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
+  port: config.DB_PORT,
 });
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Successfully connected to the database");
   let sql =
-    "CREATE TABLE IF NOT EXISTS frangipane (id INT AUTO_INCREMENT PRIMARY KEY, amandes VARCHAR(255), quantity INT)";
+    "CREATE TABLE IF NOT EXISTS obstacle (id INT AUTO_INCREMENT PRIMARY KEY, amandes VARCHAR(255), quantity INT)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table successfully created");
   });
 
   sql =
-    "CREATE TABLE IF NOT EXISTS frangipute (id INT AUTO_INCREMENT PRIMARY KEY, amandes VARCHAR(255), quantity INT)";
+    "CREATE TABLE IF NOT EXISTS background (id INT AUTO_INCREMENT PRIMARY KEY, amandes VARCHAR(255), quantity INT)";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table successfully created");
